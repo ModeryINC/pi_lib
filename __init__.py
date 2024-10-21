448eda0e088f72c9d26827afb3cabf3d98f90b7d
modery.inc <modery.inc@hothamster.pl>
2024-10-21 22:52:37 +0200

    [#7] Zmiana organizacji plików

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .Functions.base_functions import errorMsg, close, help
-from .Functions.dataInspect_functions import isPositive, canBeFloat, canBeInt
-from .Functions.dataModify_functions import gatData
-from .Classes.figures_classes import Sphere, Tetrahedron, Pyramid, Cylinder, Cone, Ellipsoid
+from .content.Functions.base_functions import errorMsg, close, help
+from .content.Functions.dataInspect_functions import isPositive, canBeFloat, canBeInt
+from .content.Functions.dataModify_functions import gatData
+from .content.Classes.figures_classes import Sphere, Tetrahedron, Pyramid, Cylinder, Cone, Ellipsoid
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pi_lib',
+    name='piLib',
     version='0.1',
     packages=find_packages(),
     description="This is a library created for tasks on the 'Podstawy informatyki'.",
