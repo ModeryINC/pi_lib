d7b2b7483bb8d8a53520dac5ef8106c546cb6d89
ModeryINC <kamykcygan@gmail.com>
2024-10-24 11:25:22 +0200

    [#18] Zmiana nazwy funkcji load_stylesheet na loadFile w init

--- pi_lib/__init__.py
+++ pi_lib/__init__.py
@@ -1,4 +1,4 @@
-from .functions.base_functions import errorMsg, close, help, load_stylesheet
+from .functions.base_functions import errorMsg, close, help, loadFile
 from .functions.dataInspect_functions import isPositive, isNegative, canBeFloat, canBeInt
 from .functions.dataModify_functions import gatData
 from .classes.figures_classes import Sphere, Tetrahedron, Pyramid, Cylinder, Cone, Ellipsoid
